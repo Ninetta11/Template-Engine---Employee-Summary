1ecae6bf36796f5bc1a2724617434a897e211f90
Ninetta11 <ninawelsh1@outlook.com>
2020-10-27 11:08:04 +1100

    Addition of writetofile code

--- app.js
+++ app.js
@@ -10,12 +10,13 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 const Employee = require("./lib/Employee");
+const { create } = require("domain");
 
 let role = "manager";
 let newEmployee = "";
 const employees = [];
 
-employeeQuestions = [
+const employeeQuestions = [
     {
         type: "input",
         message: `What is your ${role}'s name?`,
@@ -39,7 +40,7 @@ employeeQuestions = [
     }
 ]
 
-managerQuestions = [
+const managerQuestions = [
     ...employeeQuestions,
     {
         type: "input",
@@ -50,7 +51,7 @@ managerQuestions = [
     }
 ]
 
-engineerQuestions = [
+const engineerQuestions = [
     ...employeeQuestions,
     {
         type: "input",
@@ -59,7 +60,7 @@ engineerQuestions = [
     }
 ]
 
-internQuestions = [
+const internQuestions = [
     ...employeeQuestions,
     {
         type: "input",
@@ -70,7 +71,7 @@ internQuestions = [
     }
 ]
 
-selectEmployeeType = [
+const selectEmployeeType = [
     {
         type: "list",
         name: "add",
@@ -83,6 +84,18 @@ selectEmployeeType = [
     }
 ]
 
+// function to write data to team.html
+function writeToFile() {
+    let team = render(employees);
+    fs.writeFile(outputPath, team, function (error) {
+        if (error) {
+            return console.log(error)
+        }
+        console.log('success')
+    });
+}
+
+// function to create new Manager
 function newManager() {
     inquirer.prompt(managerQuestions).then(function (data) {
         newEmployee = new Manager(data.name, data.id, data.email, data.officeNumber)
@@ -92,6 +105,7 @@ function newManager() {
 
 }
 
+// function to create new Emgineer
 function newEngineer() {
     role = "Engineer";
     inquirer.prompt(engineerQuestions).then(function (data) {
@@ -101,8 +115,9 @@ function newEngineer() {
     })
 }
 
+// function to create new Intern
 function newIntern() {
-    //role = "Intern";
+    role = "Intern";
     inquirer.prompt(internQuestions).then(function (data) {
         newEmployee = new Intern(data.name, data.id, data.email, data.school)
         employees.push(newEmployee);
@@ -110,6 +125,7 @@ function newIntern() {
     })
 }
 
+// function to define Employee type
 function NewEmployee() {
     inquirer.prompt(selectEmployeeType).then(function (data) {
         switch (data.add) {
@@ -120,7 +136,7 @@ function NewEmployee() {
                 newIntern();
                 break;
             default:
-                render(employees);
+                writeToFile();
                 break;
         }
     })
@@ -134,22 +150,3 @@ function init() {
 
 // function call to initialize program
 init();
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
